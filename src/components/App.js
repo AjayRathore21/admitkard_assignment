e804e5e98df3c87180f37bf3c0e5b5d4e3d65c93
AjayRathore21 <ajaykumar420.ak79@gmail.com>
2023-09-23 00:47:33 +0530

    project setted up!

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+
+import styles from './css/signin.module.css';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      
+       <h1 className='test'>start</h1>
+
+    </div>
+  );
+}
+
+export default App;
