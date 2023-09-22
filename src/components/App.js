0d002b90778a98902344634f0e34a5e35cb895d4
AjayRathore21 <ajaykumar420.ak79@gmail.com>
2023-09-23 02:45:45 +0530

    layout completed!

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,27 @@
-
-import styles from './css/signin.module.css';
+import { useEffect, useRef } from "react";
+import styles from "./css/signin.module.css";
+import ReactIntlTelInput from "react-intl-tel-input-v2";
+import "intl-tel-input/build/css/intlTelInput.css";
+import intlTelInput from "https://cdn.skypack.dev/intl-tel-input@18.0.3";
+import MobileInput from "./MobilInput";
 
 function App() {
+  const inputRef = useRef(null);
+
+  
+
   return (
     <div className={styles.container}>
-      
-       <h1 className='test'>start</h1>
+      <header className={styles.header}>AdmitKard</header>
+      <div className={`${styles.greeting} ${styles.center}`}>
+        <span>Welcome Back!</span>
+        <br />
+        <span style={{ opacity: ".5" }}>Please sing in to your Account</span>
+      </div>
 
+      <div className={styles.input}>
+        <MobileInput />
+      </div>
     </div>
   );
 }
